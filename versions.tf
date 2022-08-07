b63b7cd39f93db35f3c1143611446b9d4f7f9b48
Omar Osman <omarosman23@aucegypt.edu>
2022-08-07 02:22:12 +0200

    first commit

--- versions.tf
+++ versions.tf
@@ -1,13 +1,5 @@
 terraform {
   
-  #backend "s3" {
-    #bucket = "terraform-state-juno"
-    #key    = "dev/s3/terraform.tfstate" 
-    #region = "us-west-2"
-    #dynamodb_table = "terraform-state-locking"
-    #encrypt = true
-  #}
-
   required_providers {
     aws = {
       source = "hashicorp/aws"
